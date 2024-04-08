1be744094cff7d2906fa68b7cc7e4ed225646953
White Tree <whitetree@Whitetreecodiinggalaxy.local>
2024-04-08 08:15:43 +0800

    tom_test

---
+++ heima-leadnews-services/admin-service/src/main/java/com/heima/admin/mapper/ChannelMapper.java
@@ -0,0 +1,10 @@
+package com.heima.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.heima.model.admin.pojo.AdChannel;
+
+/**
+ * 频道管理Mapper
+ */
+public interface ChannelMapper extends BaseMapper<AdChannel> {
+}
